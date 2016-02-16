f2674bc4e99ff0d21e88674b93d855e1c0dbbbca
Abhay Singh <Abhay Singh>
2016-02-15 21:34:04 -0600

    Changes.

--- plot1.R
+++ plot1.R
@@ -1,4 +1,14 @@
-source("DownloadData.R")
+archiveFile <- "NEI_data.zip"
+if(!file.exists(archiveFile)) {
+	archiveURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+	if(Sys.info()["sysname"] == "Darwin") {
+		download.file(url=archiveURL,destfile=archiveFile,method="curl")
+	} else {
+		download.file(url=url,destfile=archiveFile)
+	}
+}
+if(!(file.exists("summarySCC_PM25.rds") && 
+	file.exists("Source_Classification_Code.rds"))) { unzip(archiveFile) }
 
 # Load the NEI & SCC data frames.
 NEI <- readRDS("summarySCC_PM25.rds")
